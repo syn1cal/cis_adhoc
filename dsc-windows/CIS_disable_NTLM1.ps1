9804a9aa0246d826071086b081bfc35e2a3e23b4
syn1cal <23630039+syn1cal@users.noreply.github.com>
2021-03-01 13:55:12 +0000

    added Admin UAC to disable NTLM

--- dsc-windows/CIS_disable_NTLM1.ps1
+++ dsc-windows/CIS_disable_NTLM1.ps1
@@ -15,6 +15,8 @@ Configuration CIS_disable_NTLM1 {
         Name                                   = 'AccountSecurityOptions'
           # 2.3.11.7 (L1) Ensure 'Network security: LAN Manager authentication level' is set to 'Send NTLMv2 response only. Refuse LM & NTLM'
           Network_security_LAN_Manager_authentication_level = 'Send NTLMv2 responses only. Refuse LM & NTLM' 
+          # 2.3.17.1 (L1) Ensure 'User Account Control: Admin Approval Mode for the Built-in Administrator account' is set to 'Enabled' 
+          User_Account_Control_Admin_Approval_Mode_for_the_Built_in_Administrator_account = 'Enabled'
         }
 
    }
